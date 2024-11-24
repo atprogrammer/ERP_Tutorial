a7eb805d6e392478755ca66418cddb4cb16f2605
kanoksak <atprogrammer@gmail.com>
2024-11-24 10:22:17 +0700

    new1

--- frontend/components/sidebar.vue
+++ frontend/components/sidebar.vue
@@ -17,20 +17,21 @@ const toggleMenu = (menu) => {
             <img src="https://cdn.pixabay.com/photo/2024/11/07/03/12/lizard-9179598_1280.jpg" alt="avatar"
                 class="w-10 h-10 rounded-full mx-auto">
             <div class="text-center text-wite text-sm mt-3">Admin System</div>
-            <div class="text-center mt-3 flex justify-center gap-2">
-                <button class="btn btn-danger text-xs">
+            <div class="text-center mt-3">
+                <button class="btn btn-danger text-xs block w-full">
                     <i class="fa fa-sign-out mr-1"></i>
                     Sign Out
                 </button>
-                <button class="btn btn-primary text-xs">
+                <button class="btn btn-primary text-xs block w-full mt-2">
                     <i class="fa fa-user mr-1"></i>
-                    Sign Out
+                    Sign In
                 </button>
             </div>
         </div>
         <div class="sidebar-menu">
-            <NuxtLink class="nav-link" :class="{ 'active': activeMenu === 'home' }" to="/home" @click="toggleMenu('home')">
-                <i class="fa fa-home"></i>   
+            <NuxtLink class="nav-link" :class="{ 'active': activeMenu === 'home' }" to="/home"
+                @click="toggleMenu('home')">
+                <i class="fa fa-home"></i>
                 Dashboard
             </NuxtLink>
             <NuxtLink class="nav-link" :class="{ 'active': activeMenu === 'productType' }" to="/productType"
